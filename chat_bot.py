7657238f2bf1cd092de16a933c49ba0c01a7f803
Ricardo Ruiz <rasrikardo25@gmail.com>
2024-09-22 13:13:43 -0500

    se bajo un poco la temperatura

--- chat_bot.py
+++ chat_bot.py
@@ -161,7 +161,7 @@ if st.session_state.get("logged_in", False):
                     {"role": "user", "content": internal_prompt}
                 ],
                 model="llama-3.1-70b-versatile",
-                temperature=0.96,
+                temperature=0.88,
                 max_tokens=2800,
                 top_p=0.9,
                 stop=None,
