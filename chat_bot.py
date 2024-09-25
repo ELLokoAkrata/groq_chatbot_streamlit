2483bd6498074c2f9cbc9391228f53b3f6855653
Ricardo Ruiz <rasrikardo25@gmail.com>
2024-09-25 14:11:57 -0500

    se volvio al modelo original

--- chat_bot.py
+++ chat_bot.py
@@ -154,7 +154,7 @@ if st.session_state.get("logged_in", False):
                 messages=[
                     {"role": "user", "content": internal_prompt}
                 ],
-                model="llama-3.2-1b-preview",
+                model="llama-3.1-70b-versatile",
                 temperature=0.88,
                 max_tokens=2800,
                 top_p=0.9,
